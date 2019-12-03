35cdb5a3822ff87801cc20c8d28215b684d01993
Michael Katos <katoskreations@gmail.com>
2019-12-03 16:47:49 -0500

    Playing With Pug

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -6,4 +6,10 @@ router.get('/', (req, res) => {
   res.send('Hey! It works!');
 });
 
+router.get('/test', (req, res) => {
+  const wes = { name: 'Wes', age: 100, cool: true };
+
+  res.render('hello');
+});
+
 module.exports = router;
